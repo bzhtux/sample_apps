5f2f5fde996ce1b544300607b083b3a672821c74
Yannick <bzhtux@gmail.com>
2022-10-06 14:59:42 +0200

    feat(redis) Update main to use go-gin

--- redis/cmd/main.go
+++ redis/cmd/main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/bzhtux/sample_apps/redis/pkg/redis"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	version = "v0.0.1"
 )
 
 func main() {
-	fmt.Println("\033[32mTesting Golang Redis ...\033[0m")
-	redis.RedisConnect()
+	fmt.Printf("\033[32mLauching sample_app-redis %s...\033[0m\n", version)
+	redisPool := redis.NewPool()
+	rh := redis.New(redisPool)
+	gin.SetMode(gin.ReleaseMode)
+	// gin.SetMode(gin.DebugMode)
+	router := gin.Default()
+	router.MaxMultipartMemory = 16 << 32 // 16 MiB
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "Up",
+			"message": "Alive",
+		})
+	})
+	router.POST("/add", rh.AddNewKey)
+	router.GET("/get/:key", rh.GetOneKey)
+	router.DELETE("/del/:key", rh.DelOneKey)
+	router.Run(":8080")
 }
