c047ce8e9470e971dd82cc4f44e25ca9bd649536
Yannick <bzhtux@gmail.com>
2022-10-24 12:33:29 +0200

    feat(redis.models) Remove unused func for redis models, since dedicated config is used

--- redis/models/redis.go
+++ redis/models/redis.go
@@ -1,20 +1,10 @@
 package models
 
-import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/bzhtux/servicebinding/bindings"
-	"github.com/kelseyhightower/envconfig"
-	"gopkg.in/yaml.v2"
-)
-
 const (
 	CONFIG_DIR = "/config"
 )
 
-type RedisConfig struct {
+type RedisSpec struct {
 	Host       string `yaml:"host" envconfig:"REDIS_HOST"`
 	Port       int    `yaml:"port" envconnfig:"REDIS_PORT"`
 	Username   string `yaml:"username" envconfig:"REDIS_USERNAME"`
@@ -23,53 +13,59 @@ type RedisConfig struct {
 	SSLenabled bool   `yaml:"sslenabled" envconfig:"REDIS_SSL"`
 }
 
-type Config interface {
-	NewConfig()
-}
+// type Config interface {
+// 	NewConfig()
+// }
 
 type Messages struct {
-	Key   string
-	Value string
+	Key   string `json:"key" binding:"required"`
+	Value string `json:"value" binding:"required"`
 }
 
-func (rc *RedisConfig) GetConfigFromFile() {
-	file, err := os.Open(CONFIG_DIR + "/redis.yaml")
-	if err != nil {
-		log.Println("Error loading config file ...", err.Error())
-	}
-	defer file.Close()
+// func (rc *RedisConfig) GetConfigFromFile() error {
+// 	file, err := os.Open(CONFIG_DIR + "/redis.yaml")
+// 	if err != nil {
+// 		log.Println("Error loading config file ...", err.Error())
+// 		return err
+// 	}
+// 	defer file.Close()
 
-	d := yaml.NewDecoder(file)
+// 	d := yaml.NewDecoder(file)
 
-	if err := d.Decode(&rc); err != nil {
-		log.Println("Error loading config file ...", err.Error())
-	}
-}
+// 	if err := d.Decode(&rc); err != nil {
+// 		log.Println("Error loading config file ...", err.Error())
+// 		return err
+// 	}
 
-func (rc *RedisConfig) GetConfigFromEnv() {
-	envconfig.Process("", rc)
-}
+// 	return nil
+// }
 
-func (rc *RedisConfig) GetConfigFromBindings(t string) {
-	b, err := bindings.NewBinding(t)
-	if err != nil {
-		log.Printf("Error while getting bindings: %s", err.Error())
-	}
-	rc.Host = b.Host
-	fmt.Printf("Redis Host: %s\n", b.Host)
-	rc.Port = int(b.Port)
-	fmt.Printf("Redis Port: %d\n", b.Port)
-	rc.Username = b.Username
-	fmt.Printf("Redis Username: %s\n", b.Username)
-	rc.Password = b.Password
-	fmt.Printf("Redis Password: %s\n", b.Password)
-	rc.SSLenabled = b.SSL
-	fmt.Printf("Redis SSL: %v\n", b.SSL)
-}
+// func (rc *RedisConfig) GetConfigFromEnv() {
+// 	envconfig.Process("", rc)
+// }
 
-func (rc *RedisConfig) NewConfig() *RedisConfig {
-	rc.GetConfigFromFile()
-	rc.GetConfigFromBindings("redis")
-	rc.GetConfigFromEnv()
-	return rc
-}
+// func (rc *RedisConfig) GetConfigFromBindings(t string) error {
+// 	b, err := bindings.NewBinding(t)
+// 	if err != nil {
+// 		log.Printf("Error while getting bindings: %s", err.Error())
+// 		return err
+// 	}
+// 	rc.Host = b.Host
+// 	fmt.Printf("Redis Host: %s\n", b.Host)
+// 	rc.Port = int(b.Port)
+// 	fmt.Printf("Redis Port: %d\n", b.Port)
+// 	rc.Username = b.Username
+// 	fmt.Printf("Redis Username: %s\n", b.Username)
+// 	rc.Password = b.Password
+// 	fmt.Printf("Redis Password: %s\n", b.Password)
+// 	rc.SSLenabled = b.SSL
+// 	fmt.Printf("Redis SSL: %v\n", b.SSL)
+// 	return nil
+// }
+
+// func (rc *RedisConfig) NewConfig() *RedisConfig {
+// 	rc.GetConfigFromFile()
+// 	rc.GetConfigFromBindings("redis")
+// 	rc.GetConfigFromEnv()
+// 	return rc
+// }
