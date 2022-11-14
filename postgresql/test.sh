9b9ed7b8f8e0c8a33f3900772d5d906b5093d3af
Yannick <bzhtux@gmail.com>
2022-11-14 12:02:09 +0100

    feat(postgresql) Reorganize test with functions

--- postgresql/test.sh
+++ postgresql/test.sh
@@ -1,27 +1,42 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-
-HOST="app-pg.127.0.0.1.nip.io"
+HOST="${APP_NAME}.127.0.0.1.nip.io"
 PORT="80"
 
-echo "Adding a new Book: The Hitchhiker's Guide to the Galaxy by Douglas Adams"
-bookID=$(curl -sL -X POST -d '{"title": "The Hitchhiker'\'s' Guide to the Galaxy", "author": "Douglas Adams"}'  http://"${HOST}":"${PORT}"/add | jq .data.ID)
-echo "New book has ID ${bookID}"
+addBook(){
+    echo -ne "Adding a new Book: The Hitchhiker's Guide to the Galaxy by Douglas Adams\n"
+    bookID=$(curl -sL -X POST -d '{"title": "The Hitchhiker'\'s' Guide to the Galaxy", "author": "Douglas Adams"}'  http://"${HOST}":"${PORT}"/add | jq .data.ID)
+    echo -ne "New book has ID ${bookID}\n"
+}
 
+addTwiceBook(){
+    echo -ne "Adding twice the same book, expecting a conflict\n"
+    curl -sL -X POST -d '{"title": "The Hitchhiker'\'s' Guide to the Galaxy", "author": "Douglas Adams"}'  http://"${HOST}":"${PORT}"/add | jq .
+}
 
-echo ""
+getBook(){
+    echo -ne "Getting the book with ID: ${bookID}\n"
+    curl -sL http://"${HOST}":"${PORT}"/get/"${bookID}" | jq .data >&2
+}
 
-echo "Adding twice the same book, expecting a conflict"
-curl -sL -X POST -d '{"title": "The Hitchhiker'\'s' Guide to the Galaxy", "author": "Douglas Adams"}'  http://"${HOST}":"${PORT}"/add | jq .
+deleteBook(){
+    echo -ne "Deleting book with ID ${bookID}\n"
+    curl -sL -X DELETE http://"${HOST}":"${PORT}"/del/"${bookID}" | jq . >&2
+}
 
+runTests(){
+    addBook
+    echo ""
+    sleep 1
 
-echo ""
+    addTwiceBook
+    echo ""
+    sleep 1
 
-echo "Getting the book with ID: ${bookID}"
-curl -sL http://"${HOST}":"${PORT}"/get/"${bookID}" | jq .data
+    getBook
+    echo ""
+    sleep 1
 
-echo ""
-
-echo "Deleting book with ID ${bookID}"
-curl -sL -X DELETE http://"${HOST}":"${PORT}"/del/"${bookID}" | jq .
+    deleteBook
+}
